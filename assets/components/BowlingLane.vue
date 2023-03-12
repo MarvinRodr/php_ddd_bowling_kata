ff4dcad2ccc191bff16fd01f0f4fb539ffe0538c
Marvin Rodríguez <marvin.rodr.snt@gmail.com>
2023-03-12 17:15:35 +0000

    Frontend.

--- assets/components/BowlingLane.vue
+++ assets/components/BowlingLane.vue
@@ -8,7 +8,11 @@
     <div class="row">
         <div class="col-md-2">
             <strong>Player:</strong> {{player.name}}
-            <template v-if="launches.length < maxNumFrame">
+            <template v-if="showBonusLaunch() === true">
+                <button type="button" @click="makeLastLaunch" class="btn btn-info">Lucky last launch!</button>
+                <button type="button" @click="makeLastLaunch('last-launch-all-strike')" class="btn btn-warning">Make all strikes!</button>
+            </template>
+            <template v-else-if="launches.length < maxNumFrame">
                 <button type="button" @click="launch" class="btn btn-success">Lucky launch!</button>
                 <button type="button" @click="launch('strike')" class="btn btn-danger">Make a strike!</button>
                 <button type="button" @click="launch('spare')" class="btn btn-warning">Make a spare!</button>
@@ -20,7 +24,7 @@
         <div class="col-md-7">
             <div class="row row-cols-auto">
                 <template v-for="(launch, key) in launches" :key="key">
-                    <div :class="launchClasses(launch)">{{launch.first_one}} - {{ launch.second_one }}</div>
+                    <div :class="launchClasses(launch)" v-html="launchHtml(launch)"></div>
                 </template>
             </div>
         </div>
@@ -47,7 +51,9 @@ export default {
         return {
             launches: [],
             totalScore: 0,
-            maxNumFrame: 12,
+            maxNumFrame: 10,
+            maxNumPinsCanBeKnocked: 10,
+            payload: null,
             isLoading: false,
             fullPage: true,
         }
@@ -64,24 +70,23 @@ export default {
         },
         launch(type) {
             this.isLoading = true
-            let payload = null;
             switch (type) {
                 case 'strike':
-                    payload = this.getStrikePayload()
+                    this.payload = this.getStrikePayload()
                     break;
 
                 case 'spare':
-                    payload = this.getSparePayload()
-                    break;
-
-                case 'bonus':
-                    payload = this.getBonusPayload()
+                    this.payload = this.getSparePayload()
                     break;
 
                 default:
-                    payload = this.getLaunchPayload()
+                    this.payload = this.getLaunchPayload()
                     break;
             }
+
+            this.makeRequest()
+        },
+        makeRequest() {
             fetch(
                 "http://localhost:8040/api/player/launch",
                 {
@@ -90,7 +95,7 @@ export default {
                         'Accept': 'application/json',
                         'Content-Type': 'application/json'
                     },
-                    body: JSON.stringify(payload)
+                    body: JSON.stringify(this.payload)
                 }
             )
             .then((r) => r.json())
@@ -100,30 +105,53 @@ export default {
                 this.isLoading = false
             })
         },
-        payload(firstOne, secondOne) {
+        makeLastLaunch(type) {
+            switch (type) {
+                case 'last-launch-all-strike':
+                    this.payload = this.composePayload(this.maxNumPinsCanBeKnocked, this.maxNumPinsCanBeKnocked, this.maxNumPinsCanBeKnocked)
+                    break;
+                default:
+                    this.payload = this.getLastLaunchPayload()
+                    break;
+            }
+            this.makeRequest()
+        },
+        composePayload(firstOne, secondOne, thirdOne) {
             return {
                 id: null,
                 player_id: this.player.id,
                 first_one: firstOne,
                 second_one: secondOne,
-                num_frame: this.launches.length + 1,
+                third_one: thirdOne,
+                num_frame: this.launches.length < this.maxNumFrame ? this.launches.length + 1: this.maxNumFrame,
             }
         },
         getLaunchPayload() {
-            let firstOne = this.getRandomNumber(0, 10)
-            let secondOne = firstOne < 10 ? this.getRandomNumber(0, 10 - firstOne) : 0
-            return this.payload(firstOne, secondOne)
+            let firstOne = this.getRandomNumber(0, this.maxNumPinsCanBeKnocked)
+            let secondOne = firstOne < this.maxNumPinsCanBeKnocked ? this.getRandomNumber(0, this.maxNumPinsCanBeKnocked - firstOne) : 0
+            let thirdOne = 0
+            return this.composePayload(firstOne, secondOne, thirdOne)
+        },
+        getLastLaunchPayload() {
+            let firstOne = this.getRandomNumber(0, this.maxNumPinsCanBeKnocked)
+            let secondOne = 0
+            let thirdOne = 0
+            if (firstOne === this.maxNumPinsCanBeKnocked) {
+                secondOne = this.getRandomNumber(0, this.maxNumPinsCanBeKnocked)
+                if (secondOne === this.maxNumPinsCanBeKnocked) {
+                    thirdOne = this.getRandomNumber(0, this.maxNumPinsCanBeKnocked)
+                }
+            } else {
+                secondOne = firstOne < this.maxNumPinsCanBeKnocked ? this.getRandomNumber(0, this.maxNumPinsCanBeKnocked - firstOne) : 0
+            }
+
+            return this.composePayload(firstOne, secondOne, thirdOne)
         },
         getStrikePayload() {
-            return this.payload(10, 0)
+            return this.composePayload(this.maxNumPinsCanBeKnocked, 0, 0)
         },
         getSparePayload() {
-            return this.payload(0, 10)
-        },
-        getBonusPayload() {
-            let firstOne = this.getRandomNumber(0, 10)
-            let secondOne = firstOne === 10 ? this.getRandomNumber(0, 10 - firstOne) : 0
-            return this.payload(firstOne, secondOne)
+            return this.composePayload(0, this.maxNumPinsCanBeKnocked, 0)
         },
         getRandomNumber(min, max) {
             return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -135,6 +163,20 @@ export default {
                 'bg-danger': launch.is_strike,
             }
         },
+        showBonusLaunch() {
+            let lastLaunch = this.launches.at(-1)
+
+            return (lastLaunch)
+                && (this.launches.length === this.maxNumFrame)
+                && (lastLaunch.first_one === this.maxNumPinsCanBeKnocked)
+        },
+        launchHtml(launch) {
+            let first = launch.first_one ? (launch.first_one === this.maxNumPinsCanBeKnocked ? 'X' : launch.first_one) : ''
+            let second = launch.second_one ? ' - ' + (launch.second_one === this.maxNumPinsCanBeKnocked ? 'X' : launch.second_one) : ''
+            let third = launch.third_one ? ' - ' + (launch.third_one === this.maxNumPinsCanBeKnocked ? 'X' : launch.third_one) : ''
+            let launchHtml = first + second + third
+            return `<span> ${launchHtml} </span>`
+        }
     }
 }
 </script>
